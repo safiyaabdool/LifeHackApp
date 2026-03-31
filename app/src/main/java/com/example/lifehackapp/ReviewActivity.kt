a1dd6ea01b1f80ef40dc3a9bdf6e909756a2fccd
safiyaabdool <safiyaabdool1@gmail.com>
2026-03-31 09:30:25 +0300

    My third commit. Code written for Score and Review packages.

--- app/src/main/java/com/example/lifehackapp/ReviewActivity.kt
+++ app/src/main/java/com/example/lifehackapp/ReviewActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lifehackapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,23 @@ class ReviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_review)
+
+        val reviewText = findViewById<TextView>(R.id.reviewText)
+
+        val questions = listOf(
+            Question("Putting your phone in rice fixes water damage", false, "Rice does not effectively remove moisture.")
+            Question("Using a straw can prevent drinks from staining teeth", true, "Reduces contact with teeth.")
+            Question("Cracking knuckles causes arthritis", true,"Slows mold growth." )
+        )
+        val builder = StringBuilder()
+
+        for (q in question) {
+            builder.append(".${q.statement}\n")
+            builder.append("Answer: ${if (q.isHack) "Hack" else "Myth"}\n")
+            builder.append("Explanation: ${q.explanation}\n\n")
+        }
+        reviewText.text = builder.toString()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/lifehackapp/ScoreActivity.kt
+++ app/src/main/java/com/example/lifehackapp/ScoreActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lifehackapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,22 @@ class ScoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_score)
+
+        val scoreText = findViewById<TextView>(R.id.scoreText)
+        val feedbackText = findViewById<TextView>(R.id.feedbackText)
+        val reviewBtn = findViewById<Button>(R.id.reviewBtn)
+
+        val score = intent.getIntExtra("score", 0)
+        val total = intent.getIntExtra("total", 0)
+
+        scoreText.text = "Score: $score / $total"
+
+        feedbackText.text = when {
+            score == total -> "Master Hacker!"
+            score >= total / 2 -> "Great job!"
+            else -> "Stay Safe Online!"
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
